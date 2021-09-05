953ef1ab323ea1a106a67ee5102ece145f31f499
VarunAroraCode <varora97@gmail.com>
2021-09-05 15:17:36 -0700

    deleting unnecessary

---
+++ client/server.js
@@ -0,0 +1,30 @@
+const express = require('express'); 
+const axios = require('axios');
+const app = express(); 
+var cors = require('cors');
+const port = process.env.PORT || 5000; 
+app.use(cors());
+app.use(express.static('public'));
+
+
+app.get("/fruits", async (req, res) => {
+  try {
+      const response = await axios.get("https://www.fruityvice.com/api/fruit/all")
+      res.json(response.data)
+  }
+  catch (err) {
+      console.log(err)
+  }
+})
+app.get("/images", async (req, res) => {
+  try {
+      const response = await axios.get(`https://passport-media.s3-us-west-1.amazonaws.com/images/eng-intern-interview/fruit-images.json`)
+      res.json(response.data)
+  }
+  catch (err) {
+      console.log(err)
+  }
+})
+
+
+app.listen(port, () => console.log(`listening on port ${port}!`));
